d43ec56c934a851dd2bc4702f74d38659cf285d5
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-03-07 22:41:11 +0200

    Add: Header Section

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,10 +2,16 @@ import { Link } from 'react-router';
 export function Header() {
   return (
     <>
-      <header>
-        <div className="container">
+      <header className="backdrop-blur-md py-4">
+        <div className="w-[90%]  flex items-center justify-center md:justify-between flex-wrap gap-7 mx-auto">
           <div className="logo">
-            <Link></Link>
+            <Link className="md:font-bold text-4xl text-yellow-50">Weather Track</Link>
+          </div>
+          <div className="search flex flex-wrap justify-center gap-3">
+            <input type="text" placeholder="Enter country's name" className="py-2 pr-16 pl-3 bg-[#fb710037] rounded-md outline-none placeholder:text-white" />
+            <button type="button" className="bg-[#fb7100cd] px-6 py-2 rounded-md text-white font-medium text-lg">
+              Search
+            </button>
           </div>
         </div>
       </header>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { Header } from './components/layout/Header.jsx';
+import { BrowserRouter, Router } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
   </StrictMode>
 );
