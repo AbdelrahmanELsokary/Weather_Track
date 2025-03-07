c1526e66b0d7fbf7baf33f11ac5ebb4ee75d5418
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-03-07 21:44:47 +0200

    Install: React Router

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router';
+export function Header() {
+  return (
+    <>
+      <header>
+        <div className="container">
+          <div className="logo">
+            <Link></Link>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
